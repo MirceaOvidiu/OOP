8eb2969e55abcad4a90b7b5dbf8fc944fd16ce10
Dodi MIrcea Ovidiu <44977584+MirceaOvidiu@users.noreply.github.com>
2023-11-06 21:15:55 +0200

    Add files via upload

---
+++ lab5_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+class Volume
+{
+    public:
+    void succesful_action()
+    {
+        std::cout<<"area sucessfully calculated"<<std::endl;
+    }
+
+    
+}
